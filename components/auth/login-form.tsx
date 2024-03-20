849a80f0e20a6a68b58a84bb3b41357a98a6d5ff
Olabode Omoyele <yele.olabode@gmail.com>
2024-03-20 23:29:40 +0100

    2FA

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -22,6 +22,8 @@ import { login } from "@/actions/login";
 import { useTransition } from "react";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
+import { OTPInput } from "input-otp";
+import { FakeDash, Slot } from "./otp-input-components";
 
 interface LoginFormProps {}
 
@@ -29,6 +31,7 @@ const LoginForm: FC<LoginFormProps> = ({}) => {
   const [isPending, startTransition] = useTransition();
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
+  const [showTwoFactor, setShowTwoFactor] = useState(false);
 
   const searchParams = useSearchParams();
   const urlError =
@@ -41,18 +44,36 @@ const LoginForm: FC<LoginFormProps> = ({}) => {
     defaultValues: {
       email: "",
       password: "",
+      code: "",
     },
   });
 
   const onSubmit = (values: z.infer<typeof LoginSchema>) => {
     setError("");
     setSuccess("");
+    console.log(values);
 
     startTransition(() => {
-      login(values).then((data) => {
-        setError(data?.error);
-        setSuccess(data?.success);
-      });
+      login(values)
+        .then((data) => {
+          if (data?.error) {
+            if (data?.error === "Invalid code!") {
+              form.setValue("code", ""); // Reset only the code field
+            } else {
+              form.reset();
+            }
+            setError(data?.error);
+          }
+
+          if (data?.success) {
+            form.reset();
+            setSuccess(data?.success);
+          }
+          if (data?.twoFactor) {
+            setShowTwoFactor(true);
+          }
+        })
+        .catch(() => setError("Something went wrong"));
     });
   };
 
@@ -66,60 +87,91 @@ const LoginForm: FC<LoginFormProps> = ({}) => {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-6">
           <div className=" space-y-4">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      placeholder="john.doe@example.com"
-                      type="email"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      placeholder="******"
-                      type="password"
-                    />
-                  </FormControl>
-                  <Button
-                    size={"sm"}
-                    variant={"link"}
-                    asChild
-                    className="px-0 font-normal text-white"
-                  >
-                    <Link href={"/auth/reset"}>Forgot password?</Link>
-                  </Button>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            {showTwoFactor && (
+              <OTPInput
+                onComplete={(code) => {
+                  form.setValue("code", code); // Update form value with OTP code
+                  form.handleSubmit(onSubmit)(); // Submit the form using handleSubmit
+                }}
+                name="code"
+                maxLength={6}
+                containerClassName="group flex justify-center items-center has-[:disabled]:opacity-30"
+                render={({ slots }) => (
+                  <>
+                    <div className="flex">
+                      {slots.slice(0, 3).map((slot, idx) => (
+                        <Slot key={idx} {...slot} />
+                      ))}
+                    </div>
+                    <FakeDash />
+                    <div className="flex">
+                      {slots.slice(3).map((slot, idx) => (
+                        <Slot key={idx} {...slot} />
+                      ))}
+                    </div>
+                  </>
+                )}
+              />
+            )}
+            {!showTwoFactor && (
+              <>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          disabled={isPending}
+                          placeholder="john.doe@example.com"
+                          type="email"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          disabled={isPending}
+                          placeholder="******"
+                          type="password"
+                        />
+                      </FormControl>
+                      <Button
+                        size={"sm"}
+                        variant={"link"}
+                        asChild
+                        className="px-0 font-normal text-white"
+                      >
+                        <Link href={"/auth/reset"}>Forgot password?</Link>
+                      </Button>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </>
+            )}
           </div>
           <FormError message={error || urlError} />
           <FormSuccess message={success} />
+
           <Button
             disabled={isPending}
             size={"lg"}
             type="submit"
             className=" w-full"
           >
-            Login
+            {showTwoFactor ? "Confirm" : "Login"}
           </Button>
         </form>
       </Form>
